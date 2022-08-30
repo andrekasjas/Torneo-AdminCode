003488164bac38ee13a8a60e0a7e79033565b69d
ernestodelgadillopinilla <ernestoedelgadillo@hotmail.com>
2022-08-30 11:13:10 -0500

    Se ingresa la clase municipio en consola

--- Torneo.App/Torneo.App.Consola/Program.cs
+++ Torneo.App/Torneo.App.Consola/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+namespace Torneo.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+        static void Main(string[] args)
+        {
+            AddMunicipio();
+        }
+
+        private static void AddMunicipio()
+        {
+            var municipio = new Municipio
+            {
+                Nombre = "Manizales",
+            };
+            _repoMunicipio.AddMunicipio(municipio);
+        }
+    }
+}
