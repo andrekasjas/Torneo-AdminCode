e7c259aa34f6110453aaf6f98e49e221c03224c3
andrekasjas <andresmogollob@gmail.com>
2022-09-01 15:56:25 -0500

    AddDT

--- Torneo.App/Torneo.App.Consola/Program.cs
+++ Torneo.App/Torneo.App.Consola/Program.cs
@@ -5,18 +5,54 @@ namespace Torneo.App.Consola
     class Program
     {
         private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+        private static IRepositorioDT _repoDT = new RepositorioDT();
         static void Main(string[] args)
         {
-            AddMunicipio();
+            int opcion = 0;
+            do
+            {
+                Console.WriteLine("1. Insertar Municipio");
+                Console.WriteLine("2. Insertar Director Tecnico");
+                Console.WriteLine("0. Salir");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        AddMunicipio();
+                        break;
+                    case 2:
+                        AddDT();
+                        break;
+                }
+            } while (opcion != 0);
         }
 
         private static void AddMunicipio()
         {
+            Console.WriteLine("Ingrese el nombre del municipio");
+            string nombre = Console.ReadLine();
             var municipio = new Municipio
             {
-                Nombre = "Manizales",
+                Nombre = nombre,
             };
             _repoMunicipio.AddMunicipio(municipio);
+        }
+
+        private static void AddDT()
+        {
+            Console.WriteLine("Ingrese el nombre del DT");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el documento del DT");
+            string documento = Console.ReadLine();
+            Console.WriteLine("Ingrese el telefono del DT");
+            string telefono = Console.ReadLine();
+            var dt = new DirectorTecnico
+            {
+                Nombre = nombre,
+                Documento = documento,
+                Telefono = telefono,
+            };
+            _repoDT.AddDT(dt);
         }
     }
 }
