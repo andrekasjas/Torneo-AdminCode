f09b93e79332cdb4d6f2887a50953222c879a388
Angelicam9 <angelica_m9@hotmail.com>
2022-09-13 13:40:04 -0400

    detalles DT

--- Torneo.App/Torneo.App.Persistencia/AppRepositorios/IRepositorioDT.cs
+++ Torneo.App/Torneo.App.Persistencia/AppRepositorios/IRepositorioDT.cs
@@ -5,5 +5,6 @@ namespace Torneo.App.Persistencia
     {
         public DirectorTecnico AddDT(DirectorTecnico directorTecnico);
         public IEnumerable<DirectorTecnico> GetAllDTs();
+        public DirectorTecnico GetAllDTs(int IdDT);
     }
 }
