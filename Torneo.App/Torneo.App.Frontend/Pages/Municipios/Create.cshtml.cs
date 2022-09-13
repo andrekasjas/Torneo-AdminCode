d3e7b0fc89355ee7784ba1b2c62bdfb271dfbd73
andrekasjas <andresmogollob@gmail.com>
2022-09-12 20:25:17 -0500

    create Municipio

---
+++ Torneo.App/Torneo.App.Frontend/Pages/Municipios/Create.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+
+namespace Torneo.App.Frontend.Pages.Municipios
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IRepositorioMunicipio _repoMunicipio;
+        public Municipio municipio { get; set; }
+        public CreateModel(IRepositorioMunicipio repoMunicipio)
+        {
+            _repoMunicipio = repoMunicipio;
+        }
+        public void OnGet()
+        {
+            municipio = new Municipio();
+        }
+
+        public IActionResult OnPost(Municipio municipio)
+        {
+            if (ModelState.IsValid)
+            {
+                _repoMunicipio.AddMunicipio(municipio);
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
